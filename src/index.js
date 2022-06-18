a5bd50c577b0baf4790dac69fbfd6291b25223e8
Akif Aykan <akifaykan@hotmail.com>
2022-06-18 17:23:43 +0300

    proje güncelleme özelliği ile tamamlandı

--- src/index.js
+++ src/index.js
@@ -4,20 +4,25 @@ import {UI} from "./ui"
 const req = new Request("http://localhost:3000/employees")
 const ui = new UI
 
+let updateState = null
+
 class Index {
     constructor(){
         this.form = document.getElementById('employe-form')
         this.name = document.getElementById('employe-name')
         this.salary = document.getElementById('employe-salary')
         this.table = document.getElementById('employe-table')
+        this.update = document.getElementById('employe-update')
+        this.close = document.getElementById('employe-close')
 
         document.addEventListener('DOMContentLoaded', () => this.getLoadedEmployees())
+
         this.form.addEventListener('submit', (e) => this.formSubmit(e))
         this.table.addEventListener('click', (e) => this.editOrDelete(e))
+        this.update.addEventListener('click', () => this.updateEmployeesUpdate())
     }
 
     formSubmit(e){
-        e.preventDefault()
         const nameInput = this.name.value.trim()
         const salaryInput = this.salary.value.trim()
 
@@ -53,7 +58,9 @@ class Index {
         const id = tr.getAttribute('data-id')
 
         if (e.target.id === 'employe-edit'){
+            this.close.addEventListener('click', () => ui.clearInput())
             ui.editEmployeeUI(tr)
+            this.updateEmployees(tr, id)
         }
         if (e.target.id === 'employe-delete'){
             req.delete(id)
@@ -67,6 +74,30 @@ class Index {
         }
 
         e.preventDefault()
+    }
+
+    updateEmployees(tr, id){
+        updateState = {
+            updateId: id,
+            updateTr: tr
+        }
+    }
+
+    updateEmployeesUpdate(){
+        if (updateState){
+            const data = {
+                name: this.name.value.trim(),
+                salary: this.salary.value.trim()
+            }
+
+            req.put(updateState.updateId, data)
+                .then(employe => {
+                    ui.updateEmployeOnUI(employe, updateState.updateTr)
+                })
+                .catch(err => console.log(err))
+        }
+
+        ui.clearInput()
     }
 }
 
--- src/ui.js
+++ src/ui.js
@@ -5,8 +5,10 @@ export class UI {
         this.name = document.getElementById('employe-name')
         this.salary = document.getElementById('employe-salary')
         this.table = document.getElementById('employe-table')
-        this.button = document.getElementById('employe-button')
+
         this.close = document.getElementById('employe-close')
+        this.update = document.getElementById('employe-update')
+        this.button = document.getElementById('employe-button')
     }
 
     getLoadedEmployees(employees){
@@ -44,25 +46,56 @@ export class UI {
     }
 
     editEmployeeUI(tr){
-        this.button.innerHTML = 'Güncelle'
-        this.close.style.display = 'inline-flex'
-        this.close.addEventListener('click', () => this.clearInput())
+        this.close.setAttribute('data-view', 'show')
+        this.update.setAttribute('data-view', 'show')
+        this.button.setAttribute('data-view', 'hide')
 
         this.employeeToInput(tr)
     }
 
+    updateEmployeOnUI(employee, tr){
+        tr.innerHTML = `
+            <tr data-id="${employee.id}">
+                <td>${employee.id}</td>
+                <td>${employee.name}</td>
+                <td>${employee.salary}₺</td>
+                <td class="text-right flex">
+                    <a id="employe-edit" class="item_button">Düzenle</a>
+                    <a id="employe-delete" class="item_button">Sil</a>
+                </td>
+            </tr>
+        `
+    }
+
     employeeToInput(tr){
         const chidren = tr.children
-
         this.name.value = chidren[1].textContent
         this.salary.value = Number(chidren[2].textContent.replace('₺',''))
+
+        this.addEditedClass(tr)
+    }
+
+    addEditedClass(tr){
+        for (let item of this.table.children) {
+            item.classList.remove('edit')
+        }
+
+        tr.classList.add('edit')
     }
 
     clearInput(){
         this.name.value = ""
         this.salary.value = ""
-        this.close.style.display = 'none'
-        this.button.innerHTML = 'Kaydet'
+
+        if (this.close.getAttribute('data-view') === 'show'){
+            this.close.setAttribute('data-view', 'hide')
+            this.update.setAttribute('data-view', 'hide')
+            this.button.setAttribute('data-view', 'show')
+
+            for (let item of this.table.children) {
+                item.classList.remove('edit')
+            }
+        }
     }
 
     message(message, classes){
